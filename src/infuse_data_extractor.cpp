2bbd1217ad8ad44a6156b7500e730dde31862fde
Ellon PAIVA MENDES <emendes@laas.fr>
2018-12-19 15:00:29 +0100

    Add ImageExtractor functionalities to infuse_data_extractor
    While here, fix the if blocks that check if extraction options were
    informed.

--- src/infuse_data_extractor.cpp
+++ src/infuse_data_extractor.cpp
@@ -1,4 +1,5 @@
 #include "PointCloudExtractor.hpp"
+#include "ImagePairExtractor.hpp"
 
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
@@ -9,7 +10,7 @@ namespace bfs = boost::filesystem;
 void print_usage(int argc, char **argv, const bpo::options_description &desc)
 {
   std::cout << "Usage:" << '\n';
-  std::cout << "  " << argv[0] << " {--velodyne} ... <output-dir> <bag1> ... <bagN>" << '\n';
+  std::cout << "  " << argv[0] << " { --velodyne | --front | --nav | --rear } ... <output-dir> <bag1> ... <bagN>" << '\n';
   std::cout << desc << '\n';
 }
 
@@ -25,6 +26,15 @@ int main(int argc, char **argv)
       ("velodyne,v", bpo::bool_switch(), "Extract velodyne point clouds")
       ("velodyne-topic", bpo::value<std::string>()->default_value("/velodyne/point_cloud"), "Velodyne point cloud topic")
       ("velodyne-png", bpo::bool_switch(), "Extract point cloud views as pngs (Warning: this launches a PCLViewer window during extraction)")
+      ("front,f", bpo::bool_switch(), "Extract front cam images")
+      ("front-topic", bpo::value<std::string>()->default_value("/FrontCam/Stereo"), "Front cam stereo pair topic")
+      ("front-ext", bpo::value<std::string>()->default_value("pgm"), "File extension for the Front cam data")
+      ("nav,f", bpo::bool_switch(), "Extract nav cam images")
+      ("nav-topic", bpo::value<std::string>()->default_value("/NavCam/Stereo"), "Nav cam stereo pair topic")
+      ("nav-ext", bpo::value<std::string>()->default_value("pgm"), "File extension for the Nav cam data")
+      ("rear,f", bpo::bool_switch(), "Extract rear cam images")
+      ("rear-topic", bpo::value<std::string>()->default_value("/RearCam/Stereo"), "Rear cam stereo pair topic")
+      ("rear-ext", bpo::value<std::string>()->default_value("pgm"), "File extension for the Rear cam data")
       ;
 
     bpo::positional_options_description pos_desc;
@@ -52,8 +62,11 @@ int main(int argc, char **argv)
     }
 
     // Makes sure we have at least one extraction informed
-    if (not vm.count("velodyne")) {
-      std::cout << "Error: Extraction not informed. Please inform at least one data type to extact." << '\n';
+    if (not vm["velodyne"].as<bool>() and
+        not vm["front"].as<bool>() and
+        not vm["nav"].as<bool>() and
+        not vm["rear"].as<bool>()) {
+     std::cout << "Error: Extraction not informed. Please inform at least one data type to extact." << '\n';
       print_usage(argc, argv, desc);
       return 1;
     }
@@ -81,8 +94,7 @@ int main(int argc, char **argv)
       }
     }
 
-
-    if (vm.count("velodyne")) {
+    if (vm["velodyne"].as<bool>()) {
       // Create the extractor and extract clouds.
       bfs::path velodyne_output_dir = output_dir / "velodyne";
       infuse_debug_tools::PointCloudExtractor cloud_extractor{
@@ -91,10 +103,23 @@ int main(int argc, char **argv)
         vm["velodyne-topic"].as<std::string>(),
         vm["velodyne-png"].as<bool>()
       };
-
       cloud_extractor.Extract();
     }
 
+    std::array<std::string, 3> cam_names = {"front", "nav", "rear"};
+    for (const auto & cam_name : cam_names) {
+      if (vm[cam_name].as<bool>()) {
+        // Create the cam extractor and extract images.
+        bfs::path cam_output_dir = output_dir / (cam_name + "_cam");
+        infuse_debug_tools::ImagePairExtractor cam_extractor{
+          cam_output_dir.string(),
+          vm["bags"].as<std::vector<std::string>>(),
+          vm[cam_name + "-topic"].as<std::string>(),
+          vm[cam_name + "-ext"].as<std::string>()
+        };
+        cam_extractor.Extract();
+      }
+    }
 
   } catch (const bpo::error &ex) {
     std::cerr << ex.what() << '\n';
